84559e8b19eb6e4c272c434be9a8ad00647e6082
jumakiwaka <alijuma232@gmail.com>
2020-08-14 19:06:53 +0300

    add environment set up

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    mocha: true,
+  },
+  extends: ['airbnb-base'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {},
+};
